4690a36c05154286c3f6b77eb55c42ce5e628e89
Oliver Nordbjerg <hi@notbjerg.me>
2022-03-01 05:34:56 +0100

    feat: assume test

--- src/cheats/Assume.t.sol
+++ src/cheats/Assume.t.sol
@@ -7,7 +7,8 @@ import "./Cheats.sol";
 contract AssumeTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
-    function testUnimplemented() public {
-        require(false, "unimplemented");
+    function testAssume(uint8 x) public {
+        cheats.assume(x < 2 ** 7);
+        assertTrue(x < 2 ** 7, "did not discard inputs");
     }
 }
