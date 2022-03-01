2a855cdbba5d2ac93e1957c3bfc8a9455ff7a90d
Oliver Nordbjerg <hi@notbjerg.me>
2022-03-01 05:46:54 +0100

    feat: sign cheat test

--- src/cheats/Sign.t.sol
+++ src/cheats/Sign.t.sol
@@ -7,7 +7,17 @@ import "./Cheats.sol";
 contract SignTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
-    function testUnimplemented() public {
-        require(false, "unimplemented");
+    function testSignDigest(uint248 pk, bytes32 digest) public {
+        cheats.assume(pk != 0);
+
+        (uint8 v, bytes32 r, bytes32 s) = cheats.sign(pk, digest);
+        address expected = cheats.addr(pk);
+        address actual = ecrecover(digest, v, r, s);
+
+        assertEq(actual, expected, "digest signer did not match");
+    }
+
+    function testSignMessage(uint248 pk, bytes memory message) public {
+        testSignDigest(pk, keccak256(message));
     }
 }
