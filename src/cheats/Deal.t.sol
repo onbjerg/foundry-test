380860e97e2156028b02247129c051dcd39832da
Oliver Nordbjerg <hi@notbjerg.me>
2022-03-01 05:57:48 +0100

    feat: deal cheat test

--- src/cheats/Deal.t.sol
+++ src/cheats/Deal.t.sol
@@ -7,7 +7,16 @@ import "./Cheats.sol";
 contract DealTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
-    function testUnimplemented() public {
-        require(false, "unimplemented");
+    function testDeal(uint256 amount) public {
+        address target = address(1);
+        assertEq(target.balance, 0, "initial balance incorrect");
+
+        // Give half the amount
+        cheats.deal(target, amount / 2);
+        assertEq(target.balance, amount / 2, "half balance is incorrect");
+
+        // Give the entire amount to check that deal is not additive
+        cheats.deal(target, amount);
+        assertEq(target.balance, amount, "deal did not overwrite balance");
     }
 }
