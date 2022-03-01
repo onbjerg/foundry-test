de4ef0cb9f170e2b57462c2d9f362530a3eb096c
Oliver Nordbjerg <hi@notbjerg.me>
2022-03-01 05:44:36 +0100

    feat: addr cheat test

--- src/cheats/Addr.t.sol
+++ src/cheats/Addr.t.sol
@@ -7,7 +7,14 @@ import "./Cheats.sol";
 contract AddrTest is DSTest {
     Cheats constant cheats = Cheats(HEVM_ADDRESS);
 
-    function testUnimplemented() public {
-        require(false, "unimplemented");
+    function testFailPrivKeyZero() public {
+        cheats.addr(0);
+    }
+
+    function testAddr() public {
+        uint pk = 77814517325470205911140941194401928579557062014761831930645393041380819009408;
+        address expected = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+        assertEq(cheats.addr(pk), expected, "expected address did not match");
     }
 }
